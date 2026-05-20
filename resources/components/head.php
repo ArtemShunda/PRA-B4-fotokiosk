42051e5342d8ce6d8aba4202c30df8465d26fcb3
NikitaMSK <nikiperetz10@gmail.com>
2026-05-20 12:58:26 +0200

    Global update
    bankai

---
+++ resources/components/head.php
@@ -0,0 +1,8 @@
+<?php require_once __DIR__ . '/../../config/config.php'; ?>
+
+
+<meta charset="utf-8">
+<meta name="description" content="Fotokiosk">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" href="<?php echo $base_url; ?>/resources/favicon.ico" type="image/x-icon">
+<script src="https://cdn.tailwindcss.com"></script>
